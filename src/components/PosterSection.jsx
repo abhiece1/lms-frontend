d53f39057269021c9c0bbd085866a5caea49294a
abhiece1 <112398043+abhiece1@users.noreply.github.com>
2024-11-16 05:44:44 +0000

    new commit

--- src/components/PosterSection.jsx
+++ src/components/PosterSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './PosterSection.css';
+import './Postersection.css';
 
 const PosterSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
